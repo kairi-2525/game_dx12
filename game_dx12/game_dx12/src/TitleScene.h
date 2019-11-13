c3c97ac2454489184bf9e987f4b4c0729ee85eb7
kairi <kairisksv@gmail.com>
2019-11-13 16:52:47 +0900

    タイトル作成開始

--- game_dx12/game_dx12/src/TitleScene.h
+++ game_dx12/game_dx12/src/TitleScene.h
@@ -1,11 +1,88 @@
 #pragma once
 
 #include "scene.h"
+#include <unordered_map>
+#include <string>
 
 class SceneTitle : public SceneBase
 {
+private:
+	struct Model_Base
+	{
+		KDL::FLOAT3 position;
+		KDL::FLOAT3 scale;
+		KDL::FLOAT3 rotate;
+		template<class _T>
+		HRESULT Draw(KDL::DX12::App* p_app, KDL::TOOL::Camera* p_camera, const KDL::FLOAT3& light_dir, _T* p_model)
+		{
+			assert(p_model && "model nullptr");
+			assert(p_camera && "camera nullptr");
+			DirectX::XMMATRIX W;
+			{
+				DirectX::XMMATRIX S, R, T;
+				S = DirectX::XMMatrixScaling(scale.x, scale.y, scale.z);
+				R = DirectX::XMMatrixRotationRollPitchYaw(rotate.x, rotate.y, rotate.z);
+				T = DirectX::XMMatrixTranslation(position.x, position.y, position.z);
+				W = S * R * T;
+			}
+			DirectX::XMFLOAT4X4 wvp, w;
+			DirectX::XMStoreFloat4x4(&w, W);
+			p_camera->CreateUpdateWorldViewProjection(&wvp, W);
+			HRESULT hr = p_model->AddCommand(p_app->GetCommandList(0), p_app, wvp, w, { light_dir.x, light_dir.y, light_dir.z, 0.f }, { 1.f, 1.f, 1.f, 1.f });
+			return hr;
+		}
+	};
+	template<class _T>
+	struct Model
+		: public Model_Base
+	{
+		std::unique_ptr<_T> model;
+		HRESULT Draw(KDL::DX12::App* p_app, KDL::TOOL::Camera* p_camera, const KDL::FLOAT3& light_dir)
+		{
+			assert(model && "model nullptr");
+			return Model_Base::Draw<_T>(p_app, p_camera, light_dir, model.get());
+		}
+	};
+	template<class _T, class _M = Model_Base>
+	struct MenyModels
+	{
+		std::unique_ptr<_T> model;
+		std::vector<_M> data;
+		HRESULT Draw(KDL::DX12::App* p_app, KDL::TOOL::Camera* p_camera, const KDL::FLOAT3& light_dir)
+		{
+			HRESULT hr = S_OK;
+			for (auto& it : data)
+			{
+				hr = it.Model_Base::Draw<_T>(p_app, p_camera, light_dir, model.get());
+				if (FAILED(hr)) return hr;
+			}
+		}
+	};
+	struct Ground : public Model_Base
+	{
+		bool changed = false;
+	};
+	enum class GROUND_TYPE : int
+	{
+		Sand,
+		SandBroken,
+		Snow,
+		SnowBroken
+	};
+private:
+	static inline const std::unordered_map<GROUND_TYPE, std::string> grounds_list =
+	{
+		{GROUND_TYPE::Sand, "Sand"},
+		{GROUND_TYPE::SandBroken, "SandBroken"},
+		{GROUND_TYPE::Snow, "Snow"},
+		{GROUND_TYPE::SnowBroken, "SnowBroken"}
+	};
+	static inline const UINT BLOCK_NUM = 20;
+private:
+	KDL::FLOAT3 light_dir;
 	std::unique_ptr<KDL::TOOL::Camera> camera;
-	float angle;
+	Model<KDL::DX12::Mesh_FBX> player;
+	std::unordered_map<GROUND_TYPE, MenyModels<KDL::DX12::Geometric_Board, Ground>> grounds;
 public:
 	void Load(SceneManager* p_scene_mgr, KDL::Window* p_window, KDL::DX12::App* p_app);
 	void Initialize(SceneManager* p_scene_mgr, KDL::Window* p_window, KDL::DX12::App* p_app);
--- game_dx12/game_dx12/src/scene_title.cpp
+++ game_dx12/game_dx12/src/scene_title.cpp
@@ -3,6 +3,12 @@
 
 void SceneTitle::Load(SceneManager* p_scene_mgr, KDL::Window* p_window, KDL::DX12::App* p_app)
 {
+	player.model = std::make_unique<KDL::DX12::Mesh_FBX>(p_app, "./data/models/player.fbx", 1);
+
+	for (auto& it : grounds_list)
+	{
+		grounds[it.first].model = std::make_unique<KDL::DX12::Geometric_Board>(p_app, ("./data/images/Game/" + it.second + ".png"), 20);
+	}
 }
 
 void SceneTitle::Initialize(SceneManager* p_scene_mgr, KDL::Window* p_window, KDL::DX12::App* p_app)
@@ -10,23 +16,98 @@ void SceneTitle::Initialize(SceneManager* p_scene_mgr, KDL::Window* p_window, KD
 	camera =
 		std::make_unique<KDL::TOOL::Camera>(
 			KDL::TOOL::Camera(
-				{ 0.f, 10.f, -10.f },		//eye
-				{ 0.f, 0.f, 0.f },			//focus
+				{ 0.f, 1.f, -5.f },		//eye
+				{ 0.f, 1.f, 0.f },			//focus
 				{ 0.f, 1.f, 0.f },			//up
 				DirectX::XMConvertToRadians(30.f),
 				p_app->GetViewport().Width / p_app->GetViewport().Height,
 				{ 0.1f, 1000.f }
 			)
 		);
-	angle = 0;
+	light_dir = KDL::FLOAT3{ 0.f, -1.f, 1.f };
+	light_dir /= light_dir.Length();
+	player.position = KDL::FLOAT3{ 0.f, 0.8f, 0.f };
+	player.rotate = KDL::FLOAT3{ 0.f, DirectX::XMConvertToRadians(-90.f), 0.f };
+	player.scale = KDL::FLOAT3{ 0.1f, 0.1f, 0.1f };
+	for (auto& it : grounds)
+		it.second.data.clear();
+	const float size = 1.0f;
+	float set_pos = -static_cast<float>(BLOCK_NUM) / 2;
+	for (UINT i = 0u; i < BLOCK_NUM; i++)
+	{
+		auto& it = grounds[rand() % 2 == 0 ? GROUND_TYPE::Snow : GROUND_TYPE::SnowBroken].data.emplace_back();
+		it.position = KDL::FLOAT3{ set_pos + size / 2, 0, 0};
+		it.rotate = KDL::FLOAT3{ 0, 0, 0 };
+		it.scale = KDL::FLOAT3{ 1.f, 1.f, 1.f };
+		set_pos += 1.f;
+	}
 }
 
 void SceneTitle::Update(SceneManager* p_scene_mgr, KDL::Window* p_window, KDL::DX12::App* p_app)
 {
 	const auto* input = p_window->GetInput();
+	const float elpased_time = p_window->GetElapsedTime();
 	if (input->IsTrgKey(KDL::KEY_INPUTS::Enter))
 	{
 		SetNextScene<SceneLoad>();	//別スレッドでシーン切り替え
+	}
+	if (input->IsTrgKey(KDL::KEY_INPUTS::Space))
+	{
+		Initialize(p_scene_mgr, p_window, p_app);
+	}
+	const float speed = 1.f;
+	const float end_line = -10.f;
+	for (auto& g : grounds)
+	{
+		for (auto& it : g.second.data)
+		{
+			it.position.x -= speed * elpased_time;
+			if (it.position.x < end_line)
+			{
+				it = g.second.data.back();
+				g.second.data.pop_back();
+			}
+		}
+	}
+	{
+		const float break_line = -0.5f;
+
+		for (auto& it : grounds[GROUND_TYPE::Sand].data)
+		{
+			if (!it.changed && it.position.x < break_line)
+			{
+				it.changed = true;
+				grounds[GROUND_TYPE::SandBroken].data.emplace_back(it);
+				it = grounds[GROUND_TYPE::Sand].data.back();
+				grounds[GROUND_TYPE::Sand].data.pop_back();
+			}
+		}
+		for (auto& it : grounds[GROUND_TYPE::Snow].data)
+		{
+			if (!it.changed && it.position.x < break_line)
+			{
+				it.changed = true;
+				grounds[GROUND_TYPE::SnowBroken].data.emplace_back(it);
+				it = grounds[GROUND_TYPE::Snow].data.back();
+				grounds[GROUND_TYPE::Snow].data.pop_back();
+			}
+		}
+		for (auto& it : grounds[GROUND_TYPE::SandBroken].data)
+		{
+			if (!it.changed && it.position.x < break_line)
+			{
+				it = grounds[GROUND_TYPE::SandBroken].data.back();
+				grounds[GROUND_TYPE::SandBroken].data.pop_back();
+			}
+		}
+		for (auto& it : grounds[GROUND_TYPE::SnowBroken].data)
+		{
+			if (!it.changed && it.position.x < break_line)
+			{
+				it = grounds[GROUND_TYPE::SnowBroken].data.back();
+				grounds[GROUND_TYPE::SnowBroken].data.pop_back();
+			}
+		}
 	}
 }
 
@@ -34,6 +115,19 @@ void SceneTitle::Draw(SceneManager* p_scene_mgr, KDL::Window* p_window, KDL::DX1
 {
 	const KDL::COLOR4F clear_color = { 0.f, 0.f, 0.f, 1.f };
 	p_app->ClearBackBuffer(clear_color);
+	if (FAILED(player.Draw(p_app, camera.get(), light_dir)))
+	{
+		p_scene_mgr->Exit();
+		return;
+	}
+	for (auto& it : grounds)
+	{
+		if (FAILED(it.second.Draw(p_app, camera.get(), light_dir)))
+		{
+			p_scene_mgr->Exit();
+			return;
+		}
+	}
 }
 
 void SceneTitle::UnInitialize(SceneManager* p_scene_mgr, KDL::Window* p_window, KDL::DX12::App* p_app)
